bfb4792590842aa70ca1836b88af54296a7e8830
Pellé Ewann <ewann.pelle@epita.fr>
2019-02-12 20:00:53 +0100

    fixed free() and ouput

--- main.c
+++ main.c
@@ -9,19 +9,29 @@
 #include "process/image_circle.h"
 #include "process/image_scale.h"
 
+
+int test_scale(char *path)
+{
+  printf(" ==== Testing image_scale methods ====\n");
+  GdkPixbuf *image = Load_image(path);
+
+  GdkPixbuf *scale_1;
+  scale_1 = Scale_neighbors(image, 300, 300);
+  Save_pixbuf("img_out/13_Scale_01.png", "png", scale_1);
+  printf("- Scale_neighbors x300 \n");
+
+  return 0;
+}
+
 int main(int argc, char *argv[])
 {
 
   if (argc != 2)
     return 0;
-  GdkPixbuf *image = Load_image(argv[1]);
+  test_scale(argv[1]);
 
-  GdkPixbuf *scale_1;
-  scale_1 = Scale_neighbors(image, 300, 300);
-  Save_pixbuf("img_out/13_Scale_01.png", "png", scale_1);
+  printf(" ==== Results in img_out/ ==== \n");
 
-  free(image);
-  free(scale_1);
-  
   return 0;
 }
+
