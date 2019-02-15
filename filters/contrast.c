139dc092493980c4ed554aa331dfb174cfa865c0
Pellé Ewann <ewann.pelle@epita.fr>
2019-02-15 12:49:59 +0100

    contrast func added

---
+++ filters/contrast.c
@@ -0,0 +1,43 @@
+#include "contrast.h"
+
+void Brighthness(GdkPixbuf *image, int offset)
+{
+  for (int y = 0; y < gdk_pixbuf_get_height(image); ++y) {
+    for (int x = 0; x < gdk_pixbuf_get_width(image); ++x) {
+      guchar* cur = Get_pixel(image, x, y);
+      *(cur) = Truncate_256(*cur + offset);
+      *(cur + 1) = Truncate_256(*(cur + 1) + offset);
+      *(cur + 2) = Truncate_256(*(cur + 2) + offset);
+    }
+  }
+}
+
+void Contrast(GdkPixbuf *image, int offset)
+{
+  double factor = (259 * ((double)offset + 255)/(255 * (259 - (double)offset)));
+  for (int y = 0; y < gdk_pixbuf_get_height(image); ++y) {
+    for (int x = 0; x < gdk_pixbuf_get_width(image); ++x) {
+      guchar* cur = Get_pixel(image, x, y);
+      *(cur) = Truncate_256_double(factor * (*cur - 128) + 128);
+      *(cur + 1) = Truncate_256_double(factor * (*(cur + 1) - 128) + 128);
+      *(cur + 2) = Truncate_256_double(factor * (*(cur + 2) - 128) + 128);
+    }
+  }
+}
+
+void Gradient_map(GdkPixbuf *image, struct color *dark, struct color *bright)
+{
+  for (int y = 0; y < gdk_pixbuf_get_height(image); ++y) {
+    for (int x = 0; x < gdk_pixbuf_get_width(image); ++x) {
+      guchar* cur = Get_pixel(image, x, y);
+      int brightness = (cur[0] + cur[1] + cur[2]) / 3;
+      cur[0] = Truncate_256_double
+              (brightness * (bright->red - dark->red)/255 + dark->red);
+      cur[1] = Truncate_256_double
+              (brightness * (bright->green - dark->green)/255 + dark->green);
+      cur[2] = Truncate_256_double
+              (brightness * (bright->blue - dark->blue)/255 + dark->blue);
+    }
+  }
+
+}
---
+++ filters/contrast.h
@@ -0,0 +1,11 @@
+#include <gtk-3.0/gtk/gtk.h>
+#include <err.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifndef SNAPSHOP_CONTRAST_H
+#ifndef SNAPSHOP_CONTRAST_H
+
+void Brighthness(GdkPixbuf *image, int offset);
+void Contrast(GdkPixbuf *image, int offset);
+void Gradient_map(GdkPixbuf *image, struct color *dark, struct color *bright);
