699512f21b61eb4f2138feeedc65d42f888c285a
Pellé Ewann <ewann.pelle@epita.fr>
2019-03-27 12:20:11 +0100

    histogram imgs done

--- process/image_crypto.d
+++
@@ -1,2 +0,0 @@
-process/image_crypto.o: process/image_crypto.c process/image_crypto.h \
- process/image_process.h
--- process/image_process.d
+++
@@ -1,2 +0,0 @@
-process/image_process.o: process/image_process.c process/image_process.h \
- process/image_convolution.h
